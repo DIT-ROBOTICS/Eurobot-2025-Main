6e1002e3d4c02a3baebc2e919bba19d0a31889bf
TWang <wang.tw.ted@gmail.com>
2024-12-28 23:49:42 +0800

    modify: main function

--- groot_ws/src/bt_app/src/node/bt_ros2.cpp
+++ groot_ws/src/bt_app/src/node/bt_ros2.cpp
@@ -11,33 +11,34 @@ using namespace BT;
 int main(int argc, char** argv)
 {
   rclcpp::init(argc, argv);
-  auto nh = std::make_shared<rclcpp::Node>("bt_node");
-
   BehaviorTreeFactory factory;
+  auto node = std::make_shared<rclcpp::Node>("bt_node");
 
   // Register the custom node
   // TODO: Add the custom node
   BT::RosNodeParams params;
-  params.nh = nh;
-  params.default_port_value = "navigationMain";
+  params.nh = node;
+  params.default_port_value = "test";
   factory.registerNodeType<Testing>("Testing");
-  factory.registerNodeType<SimpleNavigation>("SimpleNavigation");
+  factory.registerNodeType<SimpleNavigation>("SimpleNavigation", params);
   factory.registerNodeType<TickFlow>("TickFlow");
   factory.registerNodeType<GeneratePathPoint>("GeneratePathPoint");
-  factory.registerNodeType<BT::LoopNode<geometry_msgs::msg::TwistStamped>>
-  ("LoopWayPoint");
+  factory.registerNodeType<BT::LoopNode<geometry_msgs::msg::TwistStamped>>("LoopWayPoin"
+                                                                           "t");
   factory.registerNodeType<ElapseTimeCheck>("ElapseTimeCheck");
   factory.registerNodeType<StartRace>("StartRace");
   factory.registerNodeType<RaceTimeCheck>("RaceTimeCheck");
 
   // Write tree nodes model
   std::string xml_tree_model = BT::writeTreeNodesModelXML(factory);
-  std::ofstream file("/home/user/groot_ws/groot_ws/src/bt_app/bt_config/bt_tree_node_model.xml");
+  std::ofstream file("/home/user/groot_ws/groot_ws/src/bt_app/bt_config/"
+                     "bt_tree_node_model.xml");
   file << xml_tree_model;
   file.close();
 
   // Create the tree from the XML file
-  std::string groot_xml_config_directory = "/home/user/groot_ws/groot_ws/src/bt_app/bt_config/";
+  std::string groot_xml_config_directory = "/home/user/groot_ws/groot_ws/src/bt_app/"
+                                           "bt_config/";
   for(auto const& entry : std::filesystem::directory_iterator(groot_xml_config_directory))
   {
     if(entry.path().extension() == ".xml")
@@ -50,7 +51,7 @@ int main(int argc, char** argv)
   // Create the tree
   auto tree = factory.createTree("Waypoint-Demo");
 
-  BT::Groot2Publisher publisher(tree, 2227);
+  // BT::Groot2Publisher publisher(tree, 2227);
 
   rclcpp::Rate rate(1);
   BT::NodeStatus status = BT::NodeStatus::RUNNING;
@@ -60,7 +61,7 @@ int main(int argc, char** argv)
     status = tree.rootNode()->executeTick();
 
     // Spinonce
-    rclcpp::spin_some(nh);
+    rclcpp::spin_some(node);
     rate.sleep();
   }
   return 0;
