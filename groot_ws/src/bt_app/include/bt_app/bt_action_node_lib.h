b619c505ad9614686a2d956f08851df919382df9
TWang <wang.tw.ted@gmail.com>
2024-12-28 23:50:25 +0800

    modify: RosActionNode RosTopicNode

--- groot_ws/src/bt_app/include/bt_app/bt_action_node_lib.h
+++ groot_ws/src/bt_app/include/bt_app/bt_action_node_lib.h
@@ -1,40 +1,12 @@
 #pragma once
 
-// Basic Libraries
-#include <filesystem>
-#include <fstream>
-#include <chrono>
-#include <memory>
-#include <string>
+#include <behaviortree_ros2/bt_action_node.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/logger.hpp>
 
-// ROS related Libraries
-#include "rclcpp/rclcpp.hpp"
-#include "rclcpp/executors.hpp"
+#include "btcpp_ros2_interfaces/action/navigation.hpp"
 
-// Behavior Tree Libraries
-#include "behaviortree_cpp/bt_factory.h"
-#include "behaviortree_cpp/xml_parsing.h"
-#include "behaviortree_cpp/loggers/groot2_publisher.h"
-#include "behaviortree_cpp/action_node.h"
-
-// ************* simple navigation
-// ROS Messages
-#include "geometry_msgs/msg/pose_stamped.hpp"
-#include "geometry_msgs/msg/twist.hpp"
-#include "geometry_msgs/msg/twist_stamped.hpp"
-#include "nav_msgs/msg/odometry.hpp"
-
-#include "std_msgs/msg/string.hpp"
-#include "std_msgs/msg/bool.hpp"
-#include "std_msgs/msg/int32.hpp"
-
-// TF2 Libraries
-#include "tf2/LinearMath/Quaternion.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
-#include "tf2/impl/utils.h"
-#include "tf2_ros/transform_listener.h"
-#include "tf2_ros/buffer.h"
-#include "tf2/exceptions.h"
+using namespace BT;
 
 // namespace BT
 // {
@@ -68,79 +40,33 @@ private:
   std::string input;
 };
 
+// class LocalizationTemp : public RosTopicNode<msg::Localization>
+// {
+// public:
+//   LocalizationTemp(const std::string& name, const BT::NodeConfig& config,
+//                    const RosNodeParams& params)
+//     : BT::RosTopicPubNode<geometry_msgs::msg::Twist>(name, config, params)
+//   {}
+
+// private:
+// };
+
 // Simple navigation node
-// class SimpleNavigation : public BT::RosTopicPubNode<geometry_msgs::msg::Twist>
-class SimpleNavigation : public BT::StatefulActionNode
+// class NavigationTemp : public BT::RosTopicPubNode<geometry_msgs::msg::Twist>
+class NavigationTemp : public RosActionNode<btcpp_ros2_interfaces::action::Navigation>
 {
 public:
-  // SimpleNavigation(const std::string& name, const BT::NodeConfig& config,
-  //                  const RosNodeParams& params)
-  //   : BT::RosTopicPubNode<geometry_msgs::msg::Twist>(name, config, params)
-  SimpleNavigation(const std::string& name, const BT::NodeConfig& config)
-    : BT::StatefulActionNode(name, config)
-  {
-    node_ = rclcpp::Node::make_shared("bt_action_publisher");
-    RCLCPP_INFO(node_->get_logger(), "test1");
-    publisher_ = node_->create_publisher<std_msgs::msg::String>("bt_topic", 10);
-    // **************** simple navigation
-    // Initialize ROS Publisher
-    pub_cmd_vel_ = node_->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 1);
-    // Initialize ROS Subscriber
-    sub_loc_vel_ = node_->create_subscription<nav_msgs::msg::Odometry>(
-    "/odom", 10, std::bind(&SimpleNavigation::locVelCallback, this, std::placeholders::_1));
-    // Initialize ROS Timer (25HZ)
-    // auto timer_callback = [this]() -> void {
-    //   timerCallback();
-    // };
-    // timer_ = node_->create_wall_timer(std::chrono::milliseconds(500), timer_callback);
-    timer_ = node_->create_wall_timer(std::chrono::milliseconds(40), std::bind(&SimpleNavigation::timerCallback, this));
-  }
+  NavigationTemp(const std::string& name, const NodeConfig& conf,
+                   const RosNodeParams& params)
+    : RosActionNode<btcpp_ros2_interfaces::action::Navigation>(name, conf, params)
+  {}
 
   /* Node remapping function */
-  static BT::PortsList providedPorts();
-
-  /* Start and running function */
-  BT::NodeStatus onStart();
-  BT::NodeStatus onRunning();
-
-  /* Halt function - not implemented */
-  void onHalted();
-
-  /* Finished function */
-  void setFinished();
-
-  // **************** simple navigation
-  void setGoal(geometry_msgs::msg::TwistStamped goal_position);
-
-  std::vector<std::function<void()>> finish_callbacks_;
+  static PortsList providedPorts();
+  bool setGoal(RosActionNode::Goal& goal) override;
+  NodeStatus onResultReceived(const WrappedResult& wr) override;
+  virtual NodeStatus onFailure(ActionNodeErrorCode error) override;
+  NodeStatus onFeedback(const std::shared_ptr<const Feedback> feedback);
 
 private:
-  bool finished_ = false;
-  rclcpp::Node::SharedPtr node_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-
-  // **************** simple navigation
-  /* ROS Publisher */
-  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_cmd_vel_;
-
-  /* ROS Subscriber */
-  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_loc_vel_;
-
-  /* ROS Timer */
-  rclcpp::TimerBase::SharedPtr timer_;
-
-  /* ROS Callbacks */
-  void locVelCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
-  void timerCallback(/*const rclcpp::TimerEvent& event*/);
-
-  /* ROS Items */
-  geometry_msgs::msg::Twist cmd_vel_;
-  nav_msgs::msg::Odometry loc_;
-  geometry_msgs::msg::TwistStamped goal_;
-
-  /* Other */
-  bool is_trigger_ = false;
-
-  // Time for current race (100 seconds)
-  double race_elapsed_time_ = 0;
 };
--- groot_ws/src/bt_app/src/bt_action_node_lib.cpp
+++ groot_ws/src/bt_app/src/bt_action_node_lib.cpp
@@ -1,5 +1,7 @@
 #include "bt_app/bt_action_node_lib.h"
 
+using namespace BT;
+
 // template <>
 // inline geometry_msgs::msg::TwistStamped convertFromString(BT::StringView str)
 // {
@@ -74,155 +76,183 @@ void Testing::onHalted()
   return;
 }
 
+PortsList NavigationTemp::providedPorts()
+{
+  return providedBasicPorts({ InputPort<unsigned>("order") });
+}
+
+bool NavigationTemp::setGoal(RosActionNode::Goal& goal) override
+{
+  getInput("order", goal.goal);
+  RCLCPP_INFO(logger(), "Sending Goal");
+  return true;
+}
+
+NodeStatus NavigationTemp::onResultReceived(const WrappedResult& wr) override
+{
+  RCLCPP_INFO(logger(), "Result received");
+  return NodeStatus::SUCCESS;
+}
+
+NodeStatus NavigationTemp::onFailure(ActionNodeErrorCode error) override
+{
+  return NodeStatus::FAILURE;
+}
+
+NodeStatus NavigationTemp::onFeedback(const std::shared_ptr<const Feedback> feedback)
+{
+  return NodeStatus::RUNNING;
+}
+
 // Provide the ports for the SimpleNavigation node
-BT::PortsList SimpleNavigation::providedPorts()
-{
-  return { BT::InputPort<geometry_msgs::msg::TwistStamped>("goal") };
-}
+// BT::PortsList SimpleNavigation::providedPorts()
+// {
+//   return { BT::InputPort<geometry_msgs::msg::TwistStamped>("goal") };
+// }
 
-// Start function for the SimpleNavigation node
-BT::NodeStatus SimpleNavigation::onStart()
-{
-  // Get the goal
-  RCLCPP_INFO(node_->get_logger(), "test2");
-  geometry_msgs::msg::TwistStamped goal;
-  if(!getInput<geometry_msgs::msg::TwistStamped>("goal", goal))
-  {
-    throw BT::RuntimeError("[SimpleNavigation]: Missing required input [goal]");
-  }
-  // timer_ = node_->create_wall_timer(std::chrono::milliseconds(500), std::bind(&SimpleNavigation::timerCallback, this));
+// // Start function for the SimpleNavigation node
+// BT::NodeStatus SimpleNavigation::onStart()
+// {
+//   // Get the goal
+//   RCLCPP_INFO(node_->get_logger(), "test2");
+//   geometry_msgs::msg::TwistStamped goal;
+//   if(!getInput<geometry_msgs::msg::TwistStamped>("goal", goal))
+//   {
+//     throw BT::RuntimeError("[SimpleNavigation]: Missing required input [goal]");
+//   }
+//   // timer_ = node_->create_wall_timer(std::chrono::milliseconds(500), std::bind(&SimpleNavigation::timerCallback, this));
 
-  // Set the goal
-  // kernel_->setGoal(goal);
-  setGoal(goal);
+//   // Set the goal
+//   // kernel_->setGoal(goal);
+//   setGoal(goal);
 
-  // Set the finished flag
-  finished_ = false;
+//   // Set the finished flag
+//   finished_ = false;
 
-  // Register the goal function
-  // kernel_->registerFinishCallback(std::bind(&SimpleNavigation::setFinished, this));
+//   // Register the goal function
+//   // kernel_->registerFinishCallback(std::bind(&SimpleNavigation::setFinished, this));
 
-  return BT::NodeStatus::RUNNING;
-}
+//   return BT::NodeStatus::RUNNING;
+// }
 
-// Running function for the SimpleNavigation node
-BT::NodeStatus SimpleNavigation::onRunning()
-{
-  RCLCPP_INFO(node_->get_logger(), "test3");
-  if(finished_)
-  {
-    // kernel_->clearFinishCallbacks();
-    return BT::NodeStatus::SUCCESS;
-  }
+// // Running function for the SimpleNavigation node
+// BT::NodeStatus SimpleNavigation::onRunning()
+// {
+//   RCLCPP_INFO(node_->get_logger(), "test3");
+//   if(finished_)
+//   {
+//     // kernel_->clearFinishCallbacks();
+//     return BT::NodeStatus::SUCCESS;
+//   }
 
-  // Do nothing
-  timerCallback();
-  return BT::NodeStatus::RUNNING;
-}
+//   // Do nothing
+//   timerCallback();
+//   return BT::NodeStatus::RUNNING;
+// }
 
-// Finished function for the SimpleNavigation node
-void SimpleNavigation::setFinished()
-{
-  finished_ = true;
-}
+// // Finished function for the SimpleNavigation node
+// void SimpleNavigation::setFinished()
+// {
+//   finished_ = true;
+// }
 
-// Halted function for the SimpleNavigation node
-void SimpleNavigation::onHalted()
-{
-  // kernel_->onHalted();
-  // ******************* simple navigaiton
-  // Zero the velocity
-  cmd_vel_.linear.x = 0.0;
-  cmd_vel_.angular.z = 0.0;
-  pub_cmd_vel_->publish(cmd_vel_);
+// // Halted function for the SimpleNavigation node
+// void SimpleNavigation::onHalted()
+// {
+//   // kernel_->onHalted();
+//   // ******************* simple navigaiton
+//   // Zero the velocity
+//   cmd_vel_.linear.x = 0.0;
+//   cmd_vel_.angular.z = 0.0;
+//   pub_cmd_vel_->publish(cmd_vel_);
 
-  // Set the trigger to false
-  is_trigger_ = false;
-  // **********************
-  // kernel_->clearFinishCallbacks();
+//   // Set the trigger to false
+//   is_trigger_ = false;
+//   // **********************
+//   // kernel_->clearFinishCallbacks();
 
-  std::cout << "[SimpleNavigation]: Simple Navigation Node halted\n";
+//   std::cout << "[SimpleNavigation]: Simple Navigation Node halted\n";
 
-  return;
-}
+//   return;
+// }
 
-// **************** simple navigation
-// Callback function for /loc_vel subscriber
-void SimpleNavigation::locVelCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
-{
-  // Store the message
-  loc_ = *msg;
+// // **************** simple navigation
+// // Callback function for /loc_vel subscriber
+// void SimpleNavigation::locVelCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
+// {
+//   // Store the message
+//   loc_ = *msg;
 
-  RCLCPP_INFO(node_->get_logger(), "Current Position: %f %f", loc_.pose.pose.position.x,
-              loc_.pose.pose.position.y);
-}
-// Callback function for timer
-void SimpleNavigation::timerCallback(/*const rclcpp::TimerEvent& event*/)
-{
-  printf("test4");
-  RCLCPP_INFO(node_->get_logger(), "is_trigger: %d", is_trigger_);
-  if(is_trigger_)
-  {
-    // The goal position is set
-    double dx = goal_.twist.linear.x - loc_.pose.pose.position.x;
-    double dy = goal_.twist.linear.y - loc_.pose.pose.position.y;
-    double distance = sqrt(dx * dx + dy * dy);
+//   RCLCPP_INFO(node_->get_logger(), "Current Position: %f %f", loc_.pose.pose.position.x,
+//               loc_.pose.pose.position.y);
+// }
+// // Callback function for timer
+// void SimpleNavigation::timerCallback(/*const rclcpp::TimerEvent& event*/)
+// {
+//   printf("test4");
+//   RCLCPP_INFO(node_->get_logger(), "is_trigger: %d", is_trigger_);
+//   if(is_trigger_)
+//   {
+//     // The goal position is set
+//     double dx = goal_.twist.linear.x - loc_.pose.pose.position.x;
+//     double dy = goal_.twist.linear.y - loc_.pose.pose.position.y;
+//     double distance = sqrt(dx * dx + dy * dy);
 
-    // If the distance is less than 0.1, the task is finished
-    if(distance < 0.1)
-    {
-      is_trigger_ = false;
+//     // If the distance is less than 0.1, the task is finished
+//     if(distance < 0.1)
+//     {
+//       is_trigger_ = false;
 
-      // Stop the robot
-      cmd_vel_.linear.x = 0.0;
-      cmd_vel_.angular.z = 0.0;
-      pub_cmd_vel_->publish(cmd_vel_);
+//       // Stop the robot
+//       cmd_vel_.linear.x = 0.0;
+//       cmd_vel_.angular.z = 0.0;
+//       pub_cmd_vel_->publish(cmd_vel_);
 
-      for(auto& callback : finish_callbacks_)
-      {
-        callback();
-      }
-    }
-    else
-    {
-      // Calculate the velocity (very simple local planner in nav2)
+//       for(auto& callback : finish_callbacks_)
+//       {
+//         callback();
+//       }
+//     }
+//     else
+//     {
+//       // Calculate the velocity (very simple local planner in nav2)
 
-      // Convert orientation to angle
-      tf2::Quaternion q;
-      tf2::fromMsg(loc_.pose.pose.orientation, q);
-      tf2::Matrix3x3 m(q);
-      double roll, pitch, yaw;
-      m.getRPY(roll, pitch, yaw);
+//       // Convert orientation to angle
+//       tf2::Quaternion q;
+//       tf2::fromMsg(loc_.pose.pose.orientation, q);
+//       tf2::Matrix3x3 m(q);
+//       double roll, pitch, yaw;
+//       m.getRPY(roll, pitch, yaw);
 
-      double angle = atan2(dy, dx);
-      double dangle = angle - yaw;
+//       double angle = atan2(dy, dx);
+//       double dangle = angle - yaw;
 
-      if(/* Check if we have to turn the robot */ fabs(dangle) > 0.1)
-      {
-        cmd_vel_.linear.x = 0.0;
-        cmd_vel_.angular.z = 0.3 * dangle;
-      }
-      else
-      {
-        cmd_vel_.linear.x = 0.3;
-        cmd_vel_.angular.z = 0.0;
-      }
+//       if(/* Check if we have to turn the robot */ fabs(dangle) > 0.1)
+//       {
+//         cmd_vel_.linear.x = 0.0;
+//         cmd_vel_.angular.z = 0.3 * dangle;
+//       }
+//       else
+//       {
+//         cmd_vel_.linear.x = 0.3;
+//         cmd_vel_.angular.z = 0.0;
+//       }
 
-      pub_cmd_vel_->publish(cmd_vel_);
-    }
-  }
-}
-// Set the goal position
-void SimpleNavigation::setGoal(geometry_msgs::msg::TwistStamped goal_position)
-{
-  goal_ = goal_position;
-  is_trigger_ = true;
+//       pub_cmd_vel_->publish(cmd_vel_);
+//     }
+//   }
+// }
+// // Set the goal position
+// void SimpleNavigation::setGoal(geometry_msgs::msg::TwistStamped goal_position)
+// {
+//   goal_ = goal_position;
+//   is_trigger_ = true;
 
-  // Log the twist stamped message
-  RCLCPP_INFO(node_->get_logger(), "Goal Position: %f %f ", goal_.twist.linear.x,
-              goal_.twist.linear.y);
-}
-void finished_function()
-{
-  printf("Finished!");
-}
+//   // Log the twist stamped message
+//   RCLCPP_INFO(node_->get_logger(), "Goal Position: %f %f ", goal_.twist.linear.x,
+//               goal_.twist.linear.y);
+// }
+// void finished_function()
+// {
+//   printf("Finished!");
+// }
