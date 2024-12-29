1a5da4a21fef0e73bfa58a22dce3352428756a0f
TWang <wang.tw.ted@gmail.com>
2024-12-29 15:33:56 +0800

    modify: delete unused library

--- groot_ws/src/bt_app/include/bt_app/bt_decorator_node_lib.h
+++ groot_ws/src/bt_app/include/bt_app/bt_decorator_node_lib.h
@@ -15,11 +15,6 @@
 #include "behaviortree_cpp/loggers/groot2_publisher.h"
 #include "behaviortree_cpp/decorators/loop_node.h"
 
-// ROS kernel
-#include "geometry_msgs/msg/pose_stamped.hpp"
-#include "geometry_msgs/msg/twist.hpp"
-#include "geometry_msgs/msg/twist_stamped.hpp"
-#include "nav_msgs/msg/odometry.hpp"
 
 namespace BT {
     template <> inline int convertFromString(StringView str);
