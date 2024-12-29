0b2ac722c9e1ac86b54601e5ed81153bdff565a6
TWang <wang.tw.ted@gmail.com>
2024-12-29 15:36:35 +0800

    fix: - libraries name - message syntax & type

--- groot_ws/src/bt_app/src/node/navigationServer.cpp
+++ groot_ws/src/bt_app/src/node/navigationServer.cpp
@@ -3,26 +3,26 @@
 #include <thread>
 
 // ros2
-#include "rclcpp/rclcpp.hpp"
-#include "rclcpp_action/rclcpp_action.hpp"
-#include "rclcpp_components/register_node_macro.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+#include <rclcpp_components/register_node_macro.hpp>
 // messages
-#include "std_msgs/msg/string.hpp"
-#include "std_msgs/msg/bool.hpp"
-#include "std_msgs/msg/int32.hpp"
-#include "geometry_msgs/msg/pose_stamped.hpp"
-#include "geometry_msgs/msg/twist.hpp"
-#include "geometry_msgs/msg/twist_stamped.hpp"
-#include "nav_msgs/msg/odometry.hpp"
+#include <std_msgs/msg/string.hpp>
+#include <std_msgs/msg/bool.hpp>
+#include <std_msgs/msg/int32.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <geometry_msgs/msg/twist_stamped.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 // tf2
-#include "tf2/LinearMath/Quaternion.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
-#include "tf2/impl/utils.hpp"
-#include "tf2_ros/transform_listener.hpp"
-#include "tf2_ros/buffer.hpp"
-#include "tf2/exceptions.hpp"
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tf2/impl/utils.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
+#include <tf2/exceptions.h>
 // custom action
-#include "btcpp_ros2_interfaces/action/navigation.hpp"
+#include <btcpp_ros2_interfaces/action/navigation.hpp>
 
 class TestNavServer : public rclcpp::Node {
 
@@ -47,7 +47,7 @@ private:
         std::shared_ptr<const btcpp_ros2_interfaces::action::Navigation::Goal> goal
     ) 
     {
-        RCLCPP_INFO(this->get_logger(), "Received goal request with order %d", goal->goal);
+        RCLCPP_INFO(this->get_logger(), "Received goal (%f, %f)", goal->goal.linear.x, goal->goal.linear.y);
         return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
     }
 
@@ -71,10 +71,10 @@ private:
     void botPoseCallback(const nav_msgs::msg::Odometry::SharedPtr msg)
     {
         loc_ = *msg;
-        RCLCPP_INFO(node_->get_logger(), "Current Position: %f %f", loc_.pose.pose.position.x, loc_.pose.pose.position.y);
+        RCLCPP_INFO(this->get_logger(), "Current Position: %f %f", loc_.pose.pose.position.x, loc_.pose.pose.position.y);
     }
 
-    void moveTo(auto goal_handle)
+    void moveTo(const std::shared_ptr<rclcpp_action::ServerGoalHandle<btcpp_ros2_interfaces::action::Navigation>> goal_handle)
     {
         feedback_ = std::make_shared<btcpp_ros2_interfaces::action::Navigation::Feedback>();
         is_arrived = false;
@@ -82,10 +82,10 @@ private:
         
         while(!is_arrived)
         {
-            rclcpp::spin_some(this->botPoseCallback());
+            rclcpp::spin_some(this->get_node_base_interface());
             
-            double dx = goal_.goal.twist.linear.x - loc_.pose.pose.position.x;
-            double dy = goal_.goal.twist.linear.y - loc_.pose.pose.position.y;
+            double dx = goal_.linear.x - loc_.pose.pose.position.x;
+            double dy = goal_.linear.y - loc_.pose.pose.position.y;
             double distance = sqrt(dx * dx + dy * dy);
 
             // Calculate the velocity (very simple local planner in nav2)
@@ -122,9 +122,9 @@ private:
                 pub_cmd_vel_->publish(cmd_vel_);
                 is_arrived = true;
             }
-            feedback_->feedback.twist.linear.x = loc_.pose.pose.position.x;
-            feedback_->feedback.twist.linear.y = loc_.pose.pose.position.y;
-            feedback_->feedback.twist.angular.z = yaw;
+            feedback_->feedback.linear.x = loc_.pose.pose.position.x;
+            feedback_->feedback.linear.y = loc_.pose.pose.position.y;
+            feedback_->feedback.angular.z = yaw;
             goal_handle->publish_feedback(feedback_);
             RCLCPP_INFO(this->get_logger(), "Publish feedback");
         }
@@ -132,12 +132,12 @@ private:
 
     void execute(const std::shared_ptr<rclcpp_action::ServerGoalHandle<btcpp_ros2_interfaces::action::Navigation>> goal_handle) 
     {
-        goal_ = goal_handle->get_goal();
+        goal_ = goal_handle->get_goal()->goal;
         RCLCPP_INFO(this->get_logger(), "Executing goal");
         // Initialize ROS Publisher
         pub_cmd_vel_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 1);
         // Initialize ROS Subscriber
-        sub_loc_vel_ = this->create_subscriber("/odom", 1, &TestNavServer::botPoseCallback, this);
+        sub_loc_vel_ = this->create_subscription<nav_msgs::msg::Odometry>("/odom", 10, std::bind(&TestNavServer::botPoseCallback, this, std::placeholders::_1));
         moveTo(goal_handle);
         // Initialize ROS Timer (25HZ)
         // timer_ = this->create_wall_timer(500ms, moveTo);
@@ -146,7 +146,7 @@ private:
 
         if (rclcpp::ok()) 
         {
-            result->result = 0;
+            result->result = "arrived";
             goal_handle->succeed(result);
             RCLCPP_INFO(this->get_logger(), "Goal succeeded");
         }
@@ -154,15 +154,15 @@ private:
     /* ROS Publisher */
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_cmd_vel_;
     /* ROS Subscriber */
-    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr sub_loc_vel_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_loc_vel_;
     /* ROS Timer */
     // rclcpp::TimerBase::SharedPtr timer_;
 
     /* ROS Items */
-    geometry_msgs::Twist cmd_vel_;
-    nav_msgs::Odometry loc_;
-    geometry_msgs::Twist goal_;
-    auto feedback;
+    geometry_msgs::msg::Twist cmd_vel_;
+    nav_msgs::msg::Odometry loc_;
+    geometry_msgs::msg::Twist goal_;
+    std::shared_ptr<btcpp_ros2_interfaces::action::Navigation::Feedback> feedback_;
 
     /* nav params*/
     bool is_arrived;
