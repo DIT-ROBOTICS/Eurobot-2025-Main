d5d65dd7e1e6b79589929d4f1aabfcb39fb9d812
TWang <wang.tw.ted@gmail.com>
2025-02-21 19:35:50 +0800

    debug

--- src/bt_app_2025/launch/bt_launch.py
+++ src/bt_app_2025/launch/bt_launch.py
@@ -23,7 +23,7 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             # parameters=[LaunchConfiguration('param_file')],
-            parameters=[params, {"tree_name": "NavTest"}],
+            parameters=[params, {"tree_name": "MainTree"}],
             package = 'bt_app_2025',
             executable = 'bt_m',
             name = 'bt_m'
