3164367dd043fd0d845f39b15433716bb451714b
SeanChangX <scx@gapp.nthu.edu.tw>
2025-03-31 21:47:26 +0800

    Enhance Docker entrypoint and build scripts for micro-ROS: switch to user, skip building micro_ros_msgs if already built, and update setup sourcing.

--- scripts/build_micro_ros.sh
+++ scripts/build_micro_ros.sh
@@ -10,8 +10,9 @@ git config --global --add safe.directory $ROS_WS/src/uros/micro_ros_msgs
 echo -e "\033[1;32m--------------- [ micro-ROS Build Start ] ----------------\033[0m"
 sudo apt-get update
 rosdep update
+rosdep install --from-paths src --ignore-src -y
 
-source ./install/setup.bash
+source ./install/local_setup.bash
 
 echo -e "\033[1;32m--------------- [ micro-ROS Agent Creat ] ------------------\033[0m"
 ros2 run micro_ros_setup create_agent_ws.sh
