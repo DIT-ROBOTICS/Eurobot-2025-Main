a11e1ccd31018676e48e0a97fbec3850709f5475
DITRobotics <DITROBOTICS@gapp.nthu.edu.tw>
2025-07-18 17:52:52 +0800

    modify: bot

--- src/bt_app_2025/launch/bt_launch.py
+++ src/bt_app_2025/launch/bt_launch.py
@@ -70,7 +70,7 @@ def generate_launch_description():
                 parameters=[
                     config_path,
                     map_points,
-                    {"Robot_name": "Raccoon"},  # Raccoon or Panda
+                    {"Robot_name": "Panda"},  # Raccoon or Panda
                     {"plan_code": 0} # 10e1: plan (start from 1), 10e0: color(0 for yellow, 1 for blue)
                 ],
                 package = 'startup',
